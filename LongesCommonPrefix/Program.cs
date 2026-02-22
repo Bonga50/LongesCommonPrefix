ffc67eaca356cfa96550e24fcf5833227ef788a1
Siyabonga <siyabonga4767@gmail.com>
2026-02-23 00:40:08 +0200

    Add project files.

---
+++ LongesCommonPrefix/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongesCommonPrefix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            longestCommon obj = new longestCommon();
+            string[] words = { "cir", "car" };
+            Console.WriteLine(obj.LongestCommonPrefix(words));
+
+            Console.Read();
+        }
+    }
+}
---
+++ LongesCommonPrefix/longestCommon.cs
@@ -0,0 +1,40 @@
+﻿namespace LongesCommonPrefix
+{
+    internal class longestCommon
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            string longestCommon = strs[0];
+            string finalStr = "";
+            for (int i = 1; i < strs.Length; i++)
+            {
+                for (int j = 0; j < strs[i].Length; j++)
+                {
+                    if (longestCommon.Length - 1 < j)
+                    {
+                        break;
+                    }
+
+                    if (longestCommon[j] == strs[i][j])
+                    {
+                        finalStr = finalStr + strs[i][j];
+                    }
+                    else {
+                        break;
+                    }
+
+                    
+                }
+                longestCommon = finalStr;
+                finalStr = "";
+
+            }
+
+            return longestCommon;
+        }
+    }
+
+
+
+
+}
